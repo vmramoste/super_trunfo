a10621c4850ea44aea6d28ea0ff2a0ca161fdec6
Victor Ramos <vmramos.te@gmail.com>
2025-11-07 22:27:03 -0300

    PIB per capta e densidade populacional

--- super_trunfo.c
+++ super_trunfo.c
@@ -6,18 +6,20 @@ int main(){
     char estado_1, codigo_carta_1[5], nome_cidade_1[20];
     int populacao_1, numero_pontos_turisticos_1;
     double area_km_1, pib_1;
+    float densidade_populacional_1, pib_per_capta_1;
 
     /* Variáveis para Carta 2*/
     char estado_2, codigo_carta_2[5], nome_cidade_2[20];
     int populacao_2, numero_pontos_turisticos_2;
     double area_km_2, pib_2;
+    float densidade_populacional_2, pib_per_capta_2;
 
     /* Pegar informações para Carta 1*/
     printf("Qual a letra que representa o Estado para esta carta? (Entre 'A' e 'H') ");
     scanf("%c", &estado_1);
     getchar();
     printf("Qual o código da carta? (Letra do estado e número entre 01 e 04) ");
-    fgets(codigo_carta_1, 4, stdin);
+    fgets(codigo_carta_1, 5, stdin);
     codigo_carta_1[strcspn(codigo_carta_1, "\n")] = 0;
     printf("Agora, qual o nome da cidade? "); 
     fgets(nome_cidade_1, 20, stdin); //Usamos fgets por ter espaços entre as palavras da string
@@ -37,7 +39,7 @@ int main(){
     scanf("%c", &estado_2);
     getchar();
     printf("Qual o código da carta? (Letra do estado e número entre 01 e 04) ");
-    fgets(codigo_carta_2, 4, stdin);
+    fgets(codigo_carta_2, 5, stdin);
     codigo_carta_2[strcspn(codigo_carta_2, "\n")] = 0;
     printf("Agora, qual o nome da cidade? "); 
     fgets(nome_cidade_2, 20, stdin);
@@ -51,12 +53,18 @@ int main(){
     printf("Por último, quantos pontos turísticos essa cidade têm? ");
     scanf("%d", &numero_pontos_turisticos_2);
 
+    /*Calcular Densidade Populacional e PIB per capta carta 1 e 2 */
+    densidade_populacional_1 = (float) populacao_1/area_km_1;
+    pib_per_capta_1 = (float) pib_1/populacao_1;
+    densidade_populacional_2 = (float) populacao_2/area_km_2;
+    pib_per_capta_2 = (float) pib_2/populacao_2;
+
     /* Exibição da carta 1 */
-    printf("Carta 1: \n Estado: %c \n Código: %s \n Nome Da Cidade: %s \n População: %d \n Área: %.2lf \n PIB: %.2lf bilhões de Reais \n Número de Pontos turísticos: %d",
-    estado_1, codigo_carta_1, nome_cidade_1, populacao_1, area_km_1, pib_1, numero_pontos_turisticos_1);
+    printf("Carta 1: \n Estado: %c \n Código: %s \n Nome Da Cidade: %s \n População: %d \n Área: %.2lf \n PIB: %.2lf Reais \n Número de Pontos turísticos: %d \n Densidade Populacional: %.2f \n PIB per Capta: %.2f Reais \n \n",
+    estado_1, codigo_carta_1, nome_cidade_1, populacao_1, area_km_1, pib_1, numero_pontos_turisticos_1, densidade_populacional_1, pib_per_capta_1);
 
     /* Exibição da carta 2 */
-    printf("Carta 2: \n Estado: %c \n Código: %s \n Nome Da Cidade: %s \n População: %d \n Área: %.2lf \n PIB: %.2lf bilhões de Reais \n Número de Pontos turísticos: %d",
-    estado_2, codigo_carta_2, nome_cidade_2, populacao_2, area_km_2, pib_2, numero_pontos_turisticos_2);
+    printf("Carta 2: \n Estado: %c \n Código: %s \n Nome Da Cidade: %s \n População: %d \n Área: %.2lf \n PIB: %.2lf Reais \n Número de Pontos turísticos: %d \n Densidade Populacional: %.2f \n PIB per Capta: %.2f Reais",
+    estado_2, codigo_carta_2, nome_cidade_2, populacao_2, area_km_2, pib_2, numero_pontos_turisticos_2, densidade_populacional_2, pib_per_capta_2);
 
 } 
